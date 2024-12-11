dc613b494b0e87f781745e51471f505af8e79703
NadyaNikol <nikolaenkonadin@gmail.com>
2024-12-11 18:54:39 +0200

    changed ksp to alias in build.gradle.kts, libs.versions.toml files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp")
+//    id("com.google.devtools.ksp")
+    alias(libs.plugins.ksp)
 }
 
 android {
