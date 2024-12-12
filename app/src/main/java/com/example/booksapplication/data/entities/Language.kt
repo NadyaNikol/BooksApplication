004fb3fe9e84241decffeb6836fe01670b44cd64
NadyaNikol <nikolaenkonadin@gmail.com>
2024-12-12 20:06:03 +0200

    fixed Language enum and its display in RecyclerView

--- app/src/main/java/com/example/booksapplication/data/entities/Language.kt
+++ app/src/main/java/com/example/booksapplication/data/entities/Language.kt
@@ -3,6 +3,6 @@ package com.example.booksapplication.data.entities
 /**
  * Created by Nadya N. on 10.12.2024.
  */
-enum class Language(val nameLanguage: String) {
-    EN("English"), UA("Ukrainian")
+enum class Language(val code: String) {
+    ENGLISH("en"), UKRAINIAN("uk")
 }
