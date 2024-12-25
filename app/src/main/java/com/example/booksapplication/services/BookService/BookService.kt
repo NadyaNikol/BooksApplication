3f89bd60591f64bd6cccc672b33f390e0f9491ed
NadyaNikol <nikolaenkonadin@gmail.com>
2024-12-25 15:27:15 +0200

    added BookService to implement methods from the repository

---
+++ app/src/main/java/com/example/booksapplication/services/BookService/BookService.kt
@@ -0,0 +1,28 @@
+package com.example.booksapplication.services.BookService
+
+import com.example.booksapplication.data.entities.BookEntity
+import com.example.booksapplication.data.repositories.RepositoriesLocator
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by Nadya N. on 22.12.2024.
+ */
+class BookService {
+    private val repository = RepositoriesLocator.repository
+
+    suspend fun getListEntities(): Flow<List<BookEntity>> {
+        return repository.getListEntities()
+    }
+
+    suspend fun insertEntity(bookEntity: BookEntity) {
+        return repository.insertEntity(bookEntity)
+    }
+
+    suspend fun updateEntity(bookEntity: BookEntity) {
+        return repository.updateEntity(bookEntity)
+    }
+
+    suspend fun getBookByName(name: String): Flow<List<BookEntity>> {
+        return repository.getBookByName(name)
+    }
+}
--- app/src/main/java/com/example/booksapplication/view/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/booksapplication/view/viewModels/MainViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.booksapplication.data.entities.BookEntity
-import com.example.booksapplication.data.repositories.RepositoriesLocator
+import com.example.booksapplication.services.BookService.BookService
 import kotlinx.coroutines.launch
 
 /**
@@ -13,35 +13,34 @@ import kotlinx.coroutines.launch
  */
 class MainViewModel : ViewModel() {
 
-    private val repository = RepositoriesLocator.repository
+    private val service = BookService()
 
     private var _bookLiveData: MutableLiveData<List<BookEntity>> = MutableLiveData()
     val bookLiveData: LiveData<List<BookEntity>> = _bookLiveData
 
     fun initData() {
         viewModelScope.launch {
-            repository.getListEntities().collect {
+            service.getListEntities().collect {
                 _bookLiveData.value = it
             }
-            // service.getBooksList()
         }
     }
 
     fun insert(bookEntity: BookEntity) {
         viewModelScope.launch {
-            repository.insertEntity(bookEntity)
+            service.insertEntity(bookEntity)
         }
     }
 
     fun update(bookEntity: BookEntity) {
         viewModelScope.launch {
-            repository.updateEntity(bookEntity)
+            service.updateEntity(bookEntity)
         }
     }
 
     fun searchByName(name:String){
         viewModelScope.launch {
-            repository.getBookByName(name)
+            service.getBookByName(name)
         }
     }
 
