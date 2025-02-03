ba6e2242011c41084377e04d43207d1144370ecb
NadyaNikol <nikolaenkonadin@gmail.com>
2025-02-03 18:53:20 +0200

    added androidx.test and androidx.test.ext libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.monitor)
+    implementation(libs.androidx.junit.ktx)
 
 //    implementation(libs.hibernate.validator)
 
